23a73081a30fc77b823c919c3b428473ec54352a
rajcollaboration <washimrejakhan@gmail.com>
2022-09-16 12:58:36 +0530

    section two changed

--- eden/src/Components/SectionTwo.js
+++ eden/src/Components/SectionTwo.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
 import InputGroup from 'react-bootstrap/InputGroup';
 const SectionTwo = () => {
     return (
